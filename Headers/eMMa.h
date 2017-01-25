61eb8844dd8cea5d41cbc3bc7d17420d7fed1a47
Adrián Carrera <acarrera@moddity.net>
2017-01-25 19:33:32 +0100

    upload version

--- Headers/eMMa.h
+++ Headers/eMMa.h
@@ -25,13 +25,13 @@
 Starts the session with our servers
 
 You can get your App Key from your App info. Just go to My Account > Support and create your App (if you did not do it before) and get your App Key right there. Easy!
-
+ 
 For a simple configuration put this in you AppDelegate's method:
-
-
+    
+ 
     - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
         // Override point for customization after application launch.
-
+ 
         [eMMa starteMMaSession:@"MYFANCYAPPKEY"];
         [eMMa trackLocation];
         [eMMa startPushSystem];
@@ -40,14 +40,14 @@ For a simple configuration put this in you AppDelegate's method:
 
         return YES;
     }
-
+ 
  @param appKey You app key
 */
 +(void)starteMMaSession:(NSString*)appKey;
 
 /*
- Starts the session with our servers.
-
+ Starts the session with our servers. 
+ 
   @param appKey You app key
   @param launchOptions pass the launch options on the appdelegate's didFinishLaunching method
 */
@@ -63,21 +63,21 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  If you need to see the eMMa log, enable it (before starteMMaSession)
-
+ 
  @param visible Enable log when true
  */
 +(void)setDebuggerOutput:(BOOL)visible;
 
 /**
  Enables eMMa to use the location of the user
-
+ 
  This method requieres NSLocationWhenInUseUsageDescription key defined into Info.plist file. If this key is not defined no location tracking will be enabled.
  */
 +(void)trackLocation;
 
 /**
  Configures eMMa Root View Controller. Useful to complex implementations
-
+ 
  By default eMMa uses the main window rootViewController.
  If this VC is an UINavigationController uses the first VC of the stack as the rootViewController.
  */
@@ -85,36 +85,43 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Enable or disable IDFA Tracking
-
+ 
     *IMPORTANT*
-
-    IDFA tracking is enabled by default on eMMa. If you don’t want to track with IDFA please use method [eMMa setUseIDFA:BOOL]; in order to disable.
+ 
+    IDFA tracking is enabled by default on eMMa. If you don’t want to track with IDFA please use method [eMMa setUseIDFA:BOOL]; in order to disable. 
     Anyway you need to check the speciefied box on iTunnes Connect question regarding IDFA tracking.
-
+ 
  @param use tells to eMMa when to use IDFA tracking. If NO VendorID will be used. It can be enabled/disabled for future app releases.
  */
 +(void)setUseIDFA:(BOOL)use;
 
 
-/**
+/** 
  * @name Tracking User Behaviour
- *
+ * 
  * With eMMa you can track everything. Here you’ll find the methods that allow tracking custom event that happens on your application and also log all page views.
  */
 
 /**
  Use trackEvent to count the number of times certain events happen during a session of your application.
-
+ 
  This can be useful for measuring how often users perform various actions, for example. Your application is currently limited to counting occurrences for 30 different event ids.
-
+ 
     [eMMa trackEvent:@"EVENT_TOKEN"];
-
+ 
  You can get the EVENT_TOKENS creating events on eMMa web platform, if a non-existent token is sent eMMa will return error.
-
+ 
  @param event An event token obtained from eMMa Dashboard
  */
 +(void)trackEvent:(NSString*)event;
 
+/**
+ Use setWhitelist to restrict urls that can be opened for SDK in-app communications
+ By default all urls are permited.
+ 
+ Only URLs that starts by an url in the whitelist are opened
+ */
++(void)setWhitelist:(NSArray*)urls;
 
 ///---------------------------------------------------------------------------------------
 /// @name eMMa User Stats
@@ -122,7 +129,7 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  LoginUser logs the user on eMMa database for a user_id (NSString) and email (NSString). When logged you can use [eMMa loginDefault] for log another sign in for the user with the same data.
-
+ 
  @param userId the unique id of the user
  @param mail the mail of the user
  @param extras extra fields to track
@@ -141,7 +148,7 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  RegisterUser set a complete registration from device on eMMa database for a user_id (NSString) and email (NSString)
-
+ 
  @param userId unique user identifier
  @param mail the mail of the user
  @param extras extra fields to track
@@ -153,12 +160,12 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  This method update or add extra parameters for current logged user in order to have a better segmentation data. It can be used anywhere.
-
+ 
     Example of usage:
-
+ 
     NSDictionary *extras = @{ @"key1" : @"value1", @"key2" : @"value2" };
     [eMMa trackExtraUserInfo: extras];
-
+ 
  @param info This method sends key/value information to track on the user
  */
 +(void)trackExtraUserInfo:(NSDictionary*)info;
@@ -169,7 +176,7 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Starts an order for adding products
-
+ 
  @param orderId your order id
  @param customerId your Customer ID. If not passed, eMMa will use the logged one (if exists).
  @param totalPrice your total price
@@ -185,7 +192,7 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Adds products to your current started order. Always startOrder should be called before.
-
+ 
  @param productId your product id.
  @param name your product name
  @param qty your product qty
@@ -201,7 +208,7 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Track the current order. It should be called after startOrder and after being all cart products added.
-
+ 
  The sequence of tracking order in eMMa is always startOrder>addProduct(*distinct products)>trackOrder
  */
 +(void)trackOrder;
@@ -220,11 +227,11 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Sets the current startView options
-
+ 
  Options:
     eMMaStartViewManualCall -> Sets the startView in manual mode. Useful for using startviews with labels.
                                 Also disables check for startview returning from background
-
+ 
  @param options all the options for the startview
  */
 +(void) setStartViewOptions: (eMMaStartViewOptions) options;
@@ -271,17 +278,18 @@ For a simple configuration put this in you AppDelegate's method:
  *  Use checkForBannerOnViewcontroller in order to check if show Banner added on eMMa dashboard.
  *
  *  @param viewController the UIViewController where the banner will be seen
- *	@param isShown block parameter is true if the banner is shown, else false
+ *  @param onBannerBlock  It invoked when the banner is shown or not
  */
-+(void)checkForBannerOnViewController:(UIViewController*)viewController withBlock:(eMMaOnBannerShow)onBannerBlock;
++ (void)checkForBannerOnViewController:(UIViewController*)viewController withBlock:(eMMaOnBannerShow)onBannerBlock;
 
 /**
  *   Use checkForBannerOnViewcontroller in order to check if show Banner added on eMMa dashboard. If you want you can pass a custom NSString that labels the Banner in case you use more than one Banner on your app and you need to distinguish between them.
  *
  *  @param viewController the UIViewController where the banner will be seen
  *  @param label          the label you assigned on eMMa for the Banner
+ *  @param onBannerBlock  It invoked when the banner is shown or not
  */
-+(void)checkForBannerOnViewcontroller:(UIViewController*)viewController withLabel:(NSString*)label andBlock:(eMMaOnBannerShow)onBannerBlock;
++ (void)checkForBannerOnViewcontroller:(UIViewController*)viewController withLabel:(NSString*)label andBlock:(eMMaOnBannerShow)onBannerBlock;
 
 /**
  *  Sets the parameter to autocreate the Banner when coming from background
@@ -336,13 +344,13 @@ For a simple configuration put this in you AppDelegate's method:
 
 
 
-
+ 
 ///---------------------------------------------------------------------------------------
 /// @name eMMa Tab Bar View
 ///---------------------------------------------------------------------------------------
 /**
  Sets the UITabBarController where the DynamicTab will be shown. If no UITabBarController is defined, application won't execute
-
+ 
  @param tabBarController The Application UITabBarController
  */
 +(void) setPromoTabBarController:(UITabBarController*)tabBarController;
@@ -379,63 +387,63 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Shedules an alert to rate the app
-
+ 
  eMMa makes easier to add a Rate alert in order to achieve more positive reviews app.
-
+ 
  @param appStoreURL Your AppStore app URL.
  */
 +(void)addRateAlertForAppStoreURL:(NSString*)appStoreURL;
 
 /**
  Frequency specifiying hours between alert shows
-
+ 
  By default 72 hours.
-
+ 
  @param hours  Int specifying hours between alert shows.
  */
 +(void) setRateAlertFreq: (int) hours;
 
 /**
  Configures the alert title.
-
+ 
  The default value is _Rate this app_
-
+ 
  @param title The alert title
  */
 +(void) setRateAlertTitle: (NSString*) title;
 
 /**
  Configures the alert message
-
+ 
  The default value is _If you like our app, please rate it on App Store!_
-
+ 
  @param message The alert message
  */
 +(void) setRateAlertMessage: (NSString*) message;
 
 /**
  Sets the cancel button caption
-
+ 
  The default value is _No, thanks_
-
+ 
  @param cancelButtonText The cancel caption
  */
 +(void) setRateAlertCancelButton: (NSString*) cancelButtonText;
 
 /**
  Sets the Rate Button caption
-
+ 
  The default value is _Rate it now!_
-
+ 
  @param rateItButtonText the rate button caption
  */
 +(void) setRateAlertRateItButton: (NSString*) rateItButtonText;
 
 /**
  Sets the Later button caption
-
+ 
  The default value is _Later_
-
+ 
  @param laterButtonText Later button caption
  */
 +(void) setRateAlertLaterButton: (NSString*) laterButtonText;
@@ -454,14 +462,14 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  eMMa allows you to add a very powerful push system easy to integrate. Also allows you send info through pushes and do whatever you want inside your app with it. You need to generate your certificates for your app to be compatible with the push system. Please refer to Appendix Push Notification Certificates.
-
+ 
  @param launchOptions pass the launch options on the appdelegate's didFinishLaunching method
  */
 +(void) startPushSystem: (NSDictionary*) launchOptions;
 
 /**
  This method allows to configure the behaviour of the push system.
-
+ 
  Currently the supported options are:
  eMMaPushSystemDisableAlert -> Disables showing alert messages for new pushs received.
  */
@@ -469,46 +477,46 @@ For a simple configuration put this in you AppDelegate's method:
 
 /**
  Configures the delegate for push handling
-
+ 
  @param delegate The delegate object
  */
 +(void) setPushSystemDelegate: (id<eMMaPushDelegate>)delegate;
 
 /**
  This method handles the remote notification payload
-
-
+ 
+ 
  Example of implementation:
-
+ 
     - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
         [eMMa handlePush:userInfo];
     }
-
+ 
  @param userInfo The userInfo payload
  */
 +(void) handlePush: (NSDictionary*) userInfo;
 
 /**
  This method registers a new token on eMMa servers.
-
-
+ 
+ 
  Example of implementation:
-
+ 
     -(void) application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     [eMMa registerToken:deviceToken];
     }
-
+ 
  @param deviceToken The token received from Apple Servers.
  */
 +(void)registerToken:(NSData*)deviceToken;
 
 /**
  Checks if eMMa had received any push tag in the current session
-
+ 
     eMMaPushTagBlock definition:
-
+ 
     typedef void(^eMMaPushTagBlock)(NSString* pushTag, NSString* pushTagID);
-
+ 
  @param pushTag the pattern of the pushtag to be checked
  @param block the block to be executed on pushtag received
  */
@@ -557,7 +565,7 @@ For a simple configuration put this in you AppDelegate's method:
  *  This method, sets the API URL for proxies
  *  Ex: https://www.your_proxy.com/ws/
  *
- *  @param url URL
+ *  @param url URL  
  */
 +(void)setAPIeMMaURL:(NSString*) url;
 
